5bae228bee493d951990214cb7d9141975399f39
Max Otuteye <maxotuteye12@gmail.com>
2021-12-21 18:57:57 +0000

    Initial commit

---
+++ src/main/java/max/spring/covidtrack/CovidTrackApplication.java
@@ -0,0 +1,13 @@
+package max.spring.covidtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidTrackApplication.class, args);
+    }
+
+}
