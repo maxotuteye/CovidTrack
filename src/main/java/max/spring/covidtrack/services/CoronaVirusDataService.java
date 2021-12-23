9c5bc93b3b0b0e8ad305a40affd461766fa7813a
Max Otuteye <maxotuteye12@gmail.com>
2021-12-23 12:47:08 +0000

    heroku giving issues

--- src/main/java/max/spring/covidtrack/services/CoronaVirusDataService.java
+++ src/main/java/max/spring/covidtrack/services/CoronaVirusDataService.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @Service
 public class CoronaVirusDataService {
-    private static String VIRUS_DATA_URL = "http://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
     List<LocationStats> allStats = new ArrayList<>();
 
     public List<LocationStats> getAllStats() {
